1232e30922dc4e3c164c8d805bd7ee213f3c564f
Seonghwan <tgb023620@gmail.com>
2019-12-27 14:25:55 +0900

    12.27

--- 14499.py
+++ 14499.py
@@ -9,85 +9,82 @@ south = 0
 
 MAP = []
 for i in range(N):
-	N_data = input().split()
-	N_data = list(map(int, N_data))
-	MAP.append(N_data)
-	
+    N_data = input().split()
+    N_data = list(map(int, N_data))
+    MAP.append(N_data)
+
 command = input().split()
 command = list(map(int, command))
 
 result = []
 n_c = 0
 
-while(n_c < ncommand):
-	for c in command:
-		if c == 1:
-			if x + 1 > M:
-				continue
-			if MAP[x+1][y] == 0:
-				MAP[x+1][y] = east
-				east = 0
-			else:
-				east = MAP[x+1][y]
-				MAP[x+1][y] = 0
-			temp = top
-			top = western
-			western = bottom
-			bottom = east
-			east = temp
-			result.append(top)
-			n_c += 1
-			
-		elif c == 2:
-			if x - 1 < 0:
-				continue
-			if MAP[x-1][y] == 0:
-				MAP[x-1][y] = western
-				western = 0
-			else:
-				western = MAP[x-1][y]
-				MAP[x-1][y] = 0
-			temp = top
-			top = east
-			east = bottom
-			bottom = western
-			western = temp
-			result.append(top)
-			n_c += 1
-		
-		elif c == 3:
-			if y - 1 < 0:
-				continue
-			if MAP[x][y-1] == 0:
-				MAP[x][y-1] = north
-				north = 0
-			else:
-				north = MAP[x][y-1]
-				MAP[x][y-1] = 0
-			temp = top
-			top = south
-			south = bottom
-			bottom = north
-			north = temp
-			result.append(top)
-			n_c += 1
-		
-		else:
-			if y + 1 > N:
-				continue
-			if MAP[x][y+1] == 0:
-				MAP[x][y+1] = south
-				south = 0
-			else:
-				south = MAP[x][y+1]
-				MAP[x][y+1] = 0
-			temp = top
-			top = north
-			north = bottom
-			bottom = south
-			south = temp
-			result.append(top)
-			n_c += 1
-			
+while n_c < ncommand:
+    for c in command:
+        if c == 1:
+            if y + 1 < M:
+                y = y + 1
+                temp = top
+                top = western
+                western = bottom
+                bottom = east
+                east = temp
+                if MAP[x][y] == 0:
+                    MAP[x][y] = bottom
+                else:
+                    bottom = MAP[x][y]
+                    MAP[x][y] = 0
+                result.append(top)
+            n_c += 1
+
+        elif c == 2:
+            if y - 1 >= 0:
+                y = y - 1
+                temp = top
+                top = east
+                east = bottom
+                bottom = western
+                western = temp
+                if MAP[x][y] == 0:
+                    MAP[x][y] = bottom
+                else:
+                    bottom = MAP[x][y]
+                    MAP[x][y] = 0
+                result.append(top)
+            n_c += 1
+
+        elif c == 3:
+            if x - 1 >= 0:
+                x = x - 1
+                temp = top
+                top = south
+                south = bottom
+                bottom = north
+                north = temp
+                if MAP[x][y] == 0:
+                    MAP[x][y] = bottom
+                else:
+                    bottom = MAP[x][y]
+                    MAP[x][y] = 0
+
+                result.append(top)
+            n_c += 1
+
+        else:
+            if x + 1 < N:
+                x = x + 1
+                temp = top
+                top = north
+                north = bottom
+                bottom = south
+                south = temp
+                if MAP[x][y] == 0:
+                    MAP[x][y] = bottom
+                else:
+                    bottom = MAP[x][y]
+                    MAP[x][y] = 0
+                result.append(top)
+            n_c += 1
+
 for r in result:
-	print(r)
+    print(r)
